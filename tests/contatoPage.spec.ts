5cb631d196ef14f2ef75305d821a163f71713e91
Evellin Reis <127347027+evellinreis@users.noreply.github.com>
2024-12-05 20:13:12 -0300

    Specs Finalizados

--- tests/contatoPage.spec.ts
+++ tests/contatoPage.spec.ts
@@ -1,1 +1,63 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
+import { contatoPage } from '../locators/urlWeb.json';
+import { contatoPageComponents } from '../components/contatoPageComponents.json';
+
+test('Ir para contato page da Uniao Tintas', async ({ page }) => {
+    await page.goto(contatoPage);
+    console.log('Acesso a página com sucesso!')
+});
+
+test('Verificar se existe o componente de Telefones', async ({ page }) => {
+    await page.goto(contatoPage);
+    await page.getByRole('link', { name: contatoPageComponents.telefonesComponent }).isVisible();
+    console.log('Existe o componente Telefones!')
+});
+
+test('Verificar se existe o componente Logo', async ({ page }) => {
+    await page.goto(contatoPage);
+    await page.getByRole('link', { name: contatoPageComponents.logoComponent }).isVisible();
+    console.log('Existe o componente Logo!')
+});
+
+test('Verificar se existe o componente Nome', async ({ page }) => {
+    await page.goto(contatoPage)
+    await page.getByRole('link', { name: contatoPageComponents.contatoComponent.nome }).isVisible();
+    console.log('Existe o componente Nome!')
+});
+
+
+test('Verificar se existe o componente Email', async ({ page }) => {
+    await page.goto(contatoPage)
+    await page.getByRole('link', { name: contatoPageComponents.contatoComponent.email }).isVisible();
+    console.log('Existe o componente Email!')
+});
+
+test('Verificar se existe o componente Telefone', async ({ page }) => {
+    await page.goto(contatoPage)
+    await page.getByRole('link', { name: contatoPageComponents.contatoComponent.telefone }).isVisible();
+    console.log('Existe o componente Telefone!')
+});
+
+test('Verificar se existe o componente Assunto', async ({ page }) => {
+    await page.goto(contatoPage)
+    await page.getByRole('link', { name: contatoPageComponents.contatoComponent.assunto }).isVisible();
+    console.log('Existe o componente Assunto!')
+});
+
+test('Verificar se existe o componente Mensagem', async ({ page }) => {
+    await page.goto(contatoPage)
+    await page.getByRole('link', { name: contatoPageComponents.contatoComponent.mensagem }).isVisible();
+    console.log('Existe o componente Email!')
+});
+
+test('Verificar se existe o componente Botão Enviar', async ({ page }) => {
+    await page.goto(contatoPage)
+    await page.getByRole('link', { name: contatoPageComponents.contatoComponent.botaoEnviar }).isVisible();
+    console.log('Existe o componente Email!')
+});
+
+test('Verificar se existe o componente Contato União Tintas Email', async ({ page }) => {
+    await page.goto(contatoPage);
+    await page.getByRole('link', { name: contatoPageComponents.contatoUniaoTintasEmail }).isVisible();
+    console.log('Existe o componente Contato União Tintas Email!')
+});
--- tests/empresaPage.spec.ts
+++ tests/empresaPage.spec.ts
@@ -1,1 +1,62 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
+import { empresaPage } from '../locators/urlWeb.json';
+import { empresaPageComponents } from '../components/empresaPageComponents.json';
+
+test('Ir para contato page da Uniao Tintas', async ({ page }) => {
+    await page.goto(empresaPage);
+    console.log('Acesso a página com sucesso!')
+});
+
+test('Verificar se existe o componente de Telefones', async ({ page }) => {
+    await page.goto(empresaPage);
+    await page.getByRole('link', { name: empresaPageComponents.telefonesComponent }).isVisible();
+    console.log('Existe o componente Telefones!')
+});
+
+test('Verificar se existe o componente Logo', async ({ page }) => {
+    await page.goto(empresaPage);
+    await page.getByRole('link', { name: empresaPageComponents.logoComponent }).isVisible();
+    console.log('Existe o componente Logo!')
+});
+
+test('Verificar se existe o componente Empresa', async ({ page }) => {
+    await page.goto(empresaPage);
+    await page.getByRole('link', { name: empresaPageComponents.empresaComponent }).isVisible();
+    console.log('Existe o componente Empresa!')
+});
+
+test('Verificar se existe o componente Missão', async ({ page }) => {
+    await page.goto(empresaPage);
+    await page.getByRole('link', { name: empresaPageComponents.missaoComponent }).isVisible();
+    console.log('Existe o componente Missão!')
+});
+
+test('Verificar se existe o componente UdiLoja1', async ({ page }) => {
+    await page.goto(empresaPage);
+    await page.getByRole('link', { name: empresaPageComponents.lojasComponent.udiloja1}).isVisible();
+    console.log('Existe o componente UdiLoja1!')
+});
+
+test('Verificar se existe o componente UdiLoja2', async ({ page }) => {
+    await page.goto(empresaPage);
+    await page.getByRole('link', { name: empresaPageComponents.lojasComponent.udiloja2}).isVisible();
+    console.log('Existe o componente UdiLoja2!')
+});
+
+test('Verificar se existe o componente UdiLoja3', async ({ page }) => {
+    await page.goto(empresaPage);
+    await page.getByRole('link', { name: empresaPageComponents.lojasComponent.udiloja3}).isVisible();
+    console.log('Existe o componente UdiLoja3!')
+});
+
+test('Verificar se existe o componente AraLoja1', async ({ page }) => {
+    await page.goto(empresaPage);
+    await page.getByRole('link', { name: empresaPageComponents.lojasComponent.araloja1}).isVisible();
+    console.log('Existe o componente AraLoja1!')
+});
+
+test('Verificar se existe o componente AraLoja2', async ({ page }) => {
+    await page.goto(empresaPage);
+    await page.getByRole('link', { name: empresaPageComponents.lojasComponent.araloja2}).isVisible();
+    console.log('Existe o componente AraLoja2!')
+});
